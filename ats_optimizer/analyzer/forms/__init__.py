183b6ce96ac1006ef92af54db2f25ca485fed479
sreejit chakraborty <143126776+sreejit14@users.noreply.github.com>
2025-09-13 00:55:05 +0530

    Delete ats_optimizer directory

--- ats_optimizer/analyzer/forms/__init__.py
+++
@@ -1,4 +0,0 @@
-from .upload_forms import ResumeUploadForm
-from .validation_forms import JobPostingForm
-
-__all__ = ['ResumeUploadForm', 'JobPostingForm']
--- ats_optimizer/analyzer/views/__init__.py
+++
@@ -1,9 +0,0 @@
-from .upload_views import ResumeUploadView, ProcessResumeView
-from .analysis_views import ResultsView, AnalysisHistoryView
-from .api_views import AnalysisAPIView
-
-__all__ = [
-    'ResumeUploadView', 'ProcessResumeView', 
-    'ResultsView', 'AnalysisHistoryView',
-    'AnalysisAPIView'
-]
