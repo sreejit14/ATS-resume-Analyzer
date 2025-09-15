9b6aa6a812d0d2b3f3cc99c08cac84f13c859bd6
sreejit chakraborty <143126776+sreejit14@users.noreply.github.com>
2025-09-15 11:18:36 +0530

    Update build.sh

--- ats_optimizer/build.sh
+++ ats_optimizer/build.sh
@@ -4,5 +4,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# Download the spaCy model
+python -m spacy download en_core_web_sm
+
 python manage.py collectstatic --no-input
 python manage.py migrate
